ac13e929fca2bda9e2726997dbf61254bb74ca07
benwaples <benwaples@gmail.com>
2023-04-03 17:27:48 -0700

    initial scrape

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express, { Request, Response, Express } from 'express'
 import dotenv from 'dotenv';
+import scrape from './routes/scrape'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.listen(port, () => {
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+
+app.use('/scrape', scrape)
+
 app.get('/', (req: Request, res: Response) => {
 	res.send('Express + TypeScript Server is running');
 });
 
-app.listen(port, () => {
-	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+
